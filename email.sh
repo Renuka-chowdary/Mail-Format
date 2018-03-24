dc8d7f2b6b6d65b6fbb894ab3d9b2e194d4cc272
renuka <33515288+Renuka-chowdary@users.noreply.github.com>
2018-03-24 15:55:52 +0530

    Update email.sh

--- email.sh
+++ email.sh
@@ -10,12 +10,9 @@ time=$(date +%H:%M:%S)
 echo "This is an automated daily email notification for Gitlab/Sonar/Jenkins backup and uploaded to s3 bucket on $Now at $time UTC 
 
 Backup Data Info:
-Gitlab : $(aws s3 ls --summarize --human-readable --recursive s3://backup-hi/Backups/Gitlab/2018/02/21/gitlab-backup-21-02-18 | awk '{print $3,$4}' | head -1)
-Jenkins : $(aws s3 ls --summarize --human-readable --recursive s3://backup-hi/Backups/Jenkins/daily/2018/02/21/jenkins-21-02-18 | awk '{print $3,$4}' | head -1)
-Sonar : $(aws s3 ls --summarize --human-readable --recursive s3://backup-hi/Backups/Sonar/daily/2018/02/21/sonar-backup-files-21-02-18 | awk '{print $3,$4}' | head -1)
+Gitlab : $(aws s3 ls --summarize --human-readable --recursive s3://$s3_bucket/Backups/Gitlab/$year/$month/$date/gitlab-backup-21-02-18 | awk '{print $3,$4}' | head -1)
+Jenkins : $(aws s3 ls --summarize --human-readable --recursive s3://$s3_bucket/Backups/Jenkins/$year/$month/$date/jenkins-21-02-18 | awk '{print $3,$4}' | head -1)
+Sonar : $(aws s3 ls --summarize --human-readable --recursive s3://$s3_bucket/Backups/Sonar/$year/$month/$date/sonar-backup-files-21-02-18 | awk '{print $3,$4}' | head -1)
 
 Regards,
-Devops" | mail -s "Gitlab/Jenkins/Sonar - Daily Auto Backup Success" -a "From: admin@hashedin.com" renuka.c@hashedin.com
-
-~
-
+Devops" | mail -s "Gitlab/Jenkins/Sonar - Daily Auto Backup Success" -a "From: alerts@hashedin.com" renuka.c@hashedin.com
